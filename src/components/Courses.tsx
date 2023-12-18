5750da245bb1f7c8d7fae91a065d85d793a2c61a
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-18 14:33:02 +0600

    fix: responsive issue

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -6,7 +6,7 @@ import { ICard } from "@/types/common";
 
 const Courses = () => {
   return (
-    <div className="h-screen mx-auto flex justify-center items-center">
+    <div className="min-h-screen py-10 sm:py-0 container mx-auto flex justify-center items-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-6">
         {cards?.map((card: ICard) => (
           <CourseCard key={card.id} card={card} />
